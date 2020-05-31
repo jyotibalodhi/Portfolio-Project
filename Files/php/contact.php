0bfb4dc2b30fb5e93483cde7b414cdae61f6a6d7
Jyoti Balodhi <jyotibalodhi@gmail.com>
2020-05-31 22:55:23 +0530

    updated

--- Files/php/contact.php
+++ Files/php/contact.php
@@ -1,28 +1,13 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["Name"];
-	$email = $_POST["Email"];
-	$content = $_POST["Message"];
 
-
-	$name = test_input($_POST["name"]);
-	if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-  		$nameErr = "Only letters and white space allowed";
+	$to = "jyotibalodhi@gmail.com";
+	$subject = "Form details";
+	if(isset($_POST["name"])&&isset($_POST["email"])&&isset($_POST["message"])){
+		$txt = " Name - ".$_POST["name"]. "\n Email - ".$_POST["email"]. " \n Message - ".$_POST["message"];
+		 mail($to,$subject,$txt);
+		 echo "Thank you for the message, Will write back soon!";
+	}else{
+		echo "Invalid data";
 	}
-
-	$email = test_input($_POST["email"]);
-	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  		$emailErr = "Invalid email format";
-	}
-
 	
-    $subject = "Form submission in portfolio";
-	$toEmail = "jyotibalodhi@gmail.com";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
-}
-require_once "contact.php";
 ?>
