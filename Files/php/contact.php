0acc1547af6afa2869f21da43dbd994fa4b80f6a
Jyoti Balodhi <jyotibalodhi@gmail.com>
2020-06-01 15:15:48 +0530

    Updated

--- Files/php/contact.php
+++ Files/php/contact.php
@@ -1,13 +1,13 @@
 <?php
-
+	$msg=''
 	$to = "jyotibalodhi@gmail.com";
 	$subject = "Form details";
 	if(isset($_POST["name"])&&isset($_POST["email"])&&isset($_POST["message"])){
 		$txt = " Name - ".$_POST["name"]. "\n Email - ".$_POST["email"]. " \n Message - ".$_POST["message"];
 		 mail($to,$subject,$txt);
-		 echo "Thank you for the message, Will write back soon!";
+		 $msg=  "Thank you for the message, Will write back soon!";
 	}else{
-		echo "Invalid data";
+		$msg=  "Invalid data";
 	}
-	
+	header("Location: /index.html");
 ?>
